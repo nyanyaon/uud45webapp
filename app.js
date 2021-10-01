510b64bf0d8d5f10ce854c05f99abc65c0fa7bdf
nyanyaon <nyanyaonn@gmail.com>
2021-10-02 05:04:46 +0800

    add install button

--- app.js
+++ app.js
@@ -117,3 +117,29 @@ window.addEventListener("load", () => {
     }
 });
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+let installBtn = document.getElementById("install-btn");
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+installBtn.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+});
