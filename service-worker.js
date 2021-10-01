d8fd16de8ceeb640fd2a64e0473adf45a77f9eee
nyanyaon <nyanyaonn@gmail.com>
2021-10-02 04:48:40 +0800

    add dynamic service worker

--- service-worker.js
+++ service-worker.js
@@ -8,14 +8,14 @@ const assets = [
   'https://fonts.googleapis.com/css?family=Lato:300,400,700',
 ];
 // install event
-// self.addEventListener('install', evt => {
-//   evt.waitUntil(
-//     caches.open(staticCacheName).then((cache) => {
-//       console.log('caching shell assets');
-//       cache.addAll(assets);
-//     })
-//   );
-// });
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(dynamicCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
 // activate event
 self.addEventListener('activate', evt => {
     evt.waitUntil(
