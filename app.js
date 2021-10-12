a29c0a945f3c44979eb184a0b858492e76fb3522
nyanyaon <nyanyaonn@gmail.com>
2021-10-13 00:51:10 +0800

    Fixed Animation, Finishing Content, Fixed Layout for mobile and deskktop

--- app.js
+++ app.js
@@ -1,145 +1,120 @@
 //SideBar
 function addClickMenuEvent() {
-    let btnOpen = document.getElementById("menu-btn");
-    let btnClose = document.getElementById("close-btn");
-    let sidebar = document.getElementById("sidebar");
+  let btnOpen = document.getElementById("menu-btn");
+  let btnClose = document.getElementById("close-btn");
+  let sidebar = document.getElementById("sidebar");
 
-    btnOpen.addEventListener("click", (e) => {
-        sidebar.classList.toggle("mobile-only");
-    });
+  btnOpen.addEventListener("click", (e) => {
+    sidebar.classList.toggle("mobile-only");
+  });
 
-    btnClose.addEventListener("click", (e) => {
-        sidebar.style.animation = "fadeout 0.8s ease";
-        setTimeout(()=>{
-            sidebar.style.animation = "fadein 1s ease";
-            sidebar.classList.toggle("mobile-only");
-        }, 800);
-    });
+  btnClose.addEventListener("click", (e) => {
+    sidebarAnim(sidebar);
+  });
 }
 
+let sidebarAnim = (sidebar) => {
+  sidebar.style.animationName = "out";
+  setTimeout(() => {
+    sidebar.style.animationName = "in";
+    sidebar.classList.toggle("mobile-only");
+  }, 400);
+};
 
 //Display Content
 
 async function getMenu() {
-    let response = await fetch("uud45.json");
-    const data = await response.json();
-    let el = document.getElementById("list-menu");
-    for(const prop in data) {
-        el.insertAdjacentHTML("beforeend", `<li><a href="#" data-bab="${prop}">${data[prop]["BAB"]} ${data[prop]["title"]}</a></li>`)
-    }
-    setEvent();
+  let response = await fetch("uud45.json");
+  const data = await response.json();
+  let el = document.getElementById("list-menu");
+  for (const prop in data) {
+    el.insertAdjacentHTML(
+      "beforeend",
+      `<li><a href="#" data-bab="${prop}">${data[prop]["BAB"]} ${data[prop]["title"]}</a></li>`
+    );
+  }
+  setEvent();
 }
 
 function createH(text) {
-    let h = document.createElement("h2");
-    h.innerText = "Pasal " + text;
+  let h = document.createElement("h2");
+  h.innerText = "Pasal " + text;
 
-    return h;
+  return h;
 }
 
 function createP(text, order) {
-    let p = document.createElement("p");
-    p.innerText =  `(${++order}) ${text}`;
-    p.style.cssText += `--order: ${++order}`;
+  let p = document.createElement("p");
+  p.innerText = `(${++order}) ${text}`;
+  p.style.cssText += `--order: ${++order}`;
 
-    return p;
+  return p;
 }
 
 function createPWithoutOrder(text) {
-    let p = document.createElement("p");
-    p.innerText =  `${text}`;
+  let p = document.createElement("p");
+  p.innerText = `${text}`;
+  p.style.cssText += `--order: 1`;
 
-    return p;
+  return p;
 }
 
 async function renderData(bab) {
-    let response = await fetch("uud45.json");
-    const data = await response.json();
+  let article = document.getElementById("article");
+  article.innerHTML = "";
 
-    let pasal = data[bab]["pasal"];
+  let response = await fetch("uud45.json");
+  const data = await response.json();
 
-    let article = document.getElementById("article");
-    article.innerHTML = "";
-    for(item of pasal) {
-        let el = document.createElement("div");
-        el.classList.add("pasal");
-        if(item["no"] !== "") {
-            el.appendChild(createH(item["no"]));
-        }
+  let pasal = data[bab]["pasal"];
 
-        if(item["ayat"].length === 1) {
-            el.appendChild(createPWithoutOrder(item["ayat"][0]));
-        } else {
-            for(let order in item["ayat"]) {
-                el.appendChild(createP(item["ayat"][order], order));
-            }
-        }
-        article.appendChild(el);
+  for (item of pasal) {
+    let el = document.createElement("div");
+    el.classList.add("pasal");
+    if (item["no"] !== "") {
+      el.appendChild(createH(item["no"]));
     }
+
+    if (item["ayat"].length === 1) {
+      el.appendChild(createPWithoutOrder(item["ayat"][0]));
+    } else {
+      for (let order in item["ayat"]) {
+        el.appendChild(createP(item["ayat"][order], order));
+      }
+    }
+    article.appendChild(el);
+  }
 }
 
-
 function setEvent() {
-    let nav = document.querySelector("ul.nav");
-    let lis = nav.querySelectorAll("li>a");
-    let sidebar = document.getElementById("sidebar");
+  let nav = document.querySelector("ul.nav");
+  let lis = nav.querySelectorAll("li>a");
+  let sidebar = document.getElementById("sidebar");
 
-    lis.forEach(function(li) {
-        li.addEventListener("click", function(e){
-            sidebar.style.animation = "fadeout 0.8s ease";
-            setTimeout(()=>{
-                sidebar.style.animation = "fadein 1s ease";
-                sidebar.classList.toggle("mobile-only");
-            }, 800);
-            let bab = li.dataset.bab;
-            renderData(bab);
-            focusOn(e.target);
-        });
+  lis.forEach(function (li) {
+    li.addEventListener("click", function (e) {
+      sidebarAnim(sidebar);
+      let bab = li.dataset.bab;
+      renderData(bab);
+      focusOn(e.target);
     });
+  });
 }
 
 function focusOn(e) {
-    for(let li of e.parentElement.parentElement.children) {
-        let a = li.querySelector("a");
-        a.classList.remove("selected");
-    }
-    e.classList.add("selected");
+  for (let li of e.parentElement.parentElement.children) {
+    let a = li.querySelector("a");
+    a.classList.remove("selected");
+  }
+  e.classList.add("selected");
 }
 
-document.addEventListener("DOMContentLoaded", async function(e) {
-    getMenu();
-    addClickMenuEvent();
-});
+document.addEventListener("DOMContentLoaded", async function (e) {
+  getMenu();
+  addClickMenuEvent();
 
-window.addEventListener("load", () => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("service-worker.js");
-    }
-});
-
-// Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
-let installBtn = document.getElementById("install-btn");
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
-
-window.addEventListener('appinstalled', () => {
-    installBtn.style.display = "none";
-    // Clear the deferredPrompt so it can be garbage collected
-    deferredPrompt = null;
-    // Optionally, send analytics event to indicate successful install
-    console.log("app has installed");
-});
-
-installBtn.addEventListener('click', async () => {
+  let installBtn = document.getElementById("install-btn");
+  installBtn.addEventListener("click", async () => {
     // Hide the app provided install promotion
 
     // Show the install prompt
@@ -150,4 +125,31 @@ installBtn.addEventListener('click', async () => {
     console.log(`User response to the install prompt: ${outcome}`);
     // We've used the prompt, and can't use it again, throw it away
     deferredPrompt = null;
+  });
+});
+
+// window.addEventListener("load", () => {
+//     if ("serviceWorker" in navigator) {
+//       navigator.serviceWorker.register("service-worker.js");
+//     }
+// });
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+window.addEventListener("appinstalled", () => {
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log("app has installed");
 });
--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,39 @@
-const dynamicCacheName = 'site-dynamic-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/style.css',
-  '/uud45.json',
-];
+const dynamicCacheName = "site-dynamic-v1";
+const assets = ["/", "/index.html", "/app.js", "/style.css", "/uud45.json"];
 // install event
-self.addEventListener('install', evt => {
+self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(dynamicCacheName).then((cache) => {
-      console.log('caching shell assets');
+      console.log("caching shell assets");
       cache.addAll(assets);
     })
   );
 });
 // activate event
-self.addEventListener('activate', evt => {
-    evt.waitUntil(
-      caches.keys().then(keys => {
-        return Promise.all(keys
-          .filter(key =>  key !== dynamicCacheName)
-          .map(key => caches.delete(key))
-        );
-      })
-    );
-  });
-  // fetch event
-  self.addEventListener('fetch', evt => {
-    evt.respondWith(
-      caches.match(evt.request).then(cacheRes => {
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCacheName).then(cache => {
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCacheName).then((cache) => {
             cache.put(evt.request.url, fetchRes.clone());
             return fetchRes;
-          })
-        });
-      })
-    );
-  });
+          });
+        })
+      );
+    })
+  );
+});
